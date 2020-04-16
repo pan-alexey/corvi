c76fa5664930bbb4c5c525dc4799be64c3412e41
Pan Alexey <pan-alexey@ozon.ru>
2020-04-16 15:28:14 +0300

    add todo

--- src/core/builder.ts
+++ src/core/builder.ts
@@ -6,27 +6,33 @@
 // builder.series
 // builder.task
 
+// builder.run([], immutable=false); if immutable = trye, state will clone every
+
+
+// default state = [[a],[b],[c [d], e]]
+// index => current task index map for state (Example for [d] return [2,0])
+
 /*
 
-const A = builder.task(()=>{
+const A = builder.task((state, index)=>{
   return new Promise((resolve)=>{
     resolve("ok A");
   });
 });
 
-const B = builder.task(()=>{
+const B = builder.task((state)=>{
   return new Promise((resolve)=>{
     resolve("ok A");
   });
 });
 
-const C = builder.task(()=>{
+const C = builder.task((state)=>{
   return new Promise((resolve)=>{
     resolve("ok C");
   });
 });
 
-const D = builder.task(()=>{
+const D = builder.task((state)=>{
   return new Promise((resolve)=>{
     resolve("ok D");
   });
@@ -34,18 +40,18 @@ const D = builder.task(()=>{
 
 const series = {};
 
-series['A,B'] = builder.series(
-  builder.paralel(A, B),
-  builder.paralel(B, C),
-);
+series['A,B'] = builder.series([
+  builder.paralel([A, B]),
+  builder.paralel(B, C]),
+]);
 
-builder.run(
+builder.run([
   series['A,B']
   builder.paralell(C, D),
   builder.series(
     builder.paralel(A, B),
     builder.paralel(B, C),
-  );
-);
+  )
+], immutable);
 
 */
